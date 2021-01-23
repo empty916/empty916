084929c3e1e0eebebef2f2d9ddb3c7e8dbc2f46f
empty916 <empty916@qq.com>
2021-01-23 14:38:18 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "Empty916",
+  title: "EMPTY",
   description: "Just playing around",
   locales: {
     // 键名是该语言所属的子路径
@@ -8,14 +8,14 @@ module.exports = {
       selectText: "Languages",
       label: "English",
       lang: "en-US", // 将会被设置为 <html> 的 lang 属性
-      title: "Empty916",
-      description: "empty916 site",
+      title: "EMPTY",
+      description: "empty site",
     },
     "/zh/": {
       selectText: "选择语言",
       label: "简体中文",
       lang: "zh-CN",
-      title: "Empty916",
+      title: "EMPTY",
       description: "empty916的站",
     },
   },
